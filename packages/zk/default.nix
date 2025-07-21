ee85d91d3eaa3ccc73844851b58c0d3c3b608603
rx342 <15484682+rx342@users.noreply.github.com>
2025-07-21 11:39:47 +0200

    fix(zk): expand home

--- packages/zk/default.nix
+++ packages/zk/default.nix
@@ -7,7 +7,7 @@
       pname = "${old.pname}-rx342";
     };
     env = {
-      ZK_NOTEBOOK_DIR.value = "~/zk_notes";
+      ZK_NOTEBOOK_DIR.value = "${builtins.getEnv "HOME"}/zk_notes";
       XDG_CONFIG_HOME = {
         value = ./.;
         force = true;
