b05e293dc446ee00491251347f1797f7a062fd64
rx342 <15484682+rx342@users.noreply.github.com>
2025-09-19 19:03:41 +0200

    feat(apple-music): make a whole derivation

--- packages/apple-music/default.nix
+++ packages/apple-music/default.nix
@@ -1,4 +1,5 @@
 {
+  stdenvNoCC,
   makeDesktopItem,
   writeShellApplication,
   chromium,
@@ -15,11 +16,21 @@ let
         chromium --user-data-dir="$HOME"/.local/share/chromium-apple-music --app=https://music.apple.com/fr
       '';
   };
+  desktopItem = makeDesktopItem {
+    name = "apple-music";
+    desktopName = "Apple Music";
+    exec = "${apple-music}/bin/apple-music";
+    terminal = false;
+    type = "Application";
+  };
 in
-makeDesktopItem {
-  name = "apple-music";
-  desktopName = "Apple Music";
-  exec = "${apple-music}/bin/apple-music";
-  terminal = false;
-  type = "Application";
+stdenvNoCC.mkDerivation {
+  pname = "apple-music";
+  version = "1.0";
+  dontUnpack = true;
+  installPhase = ''
+    mkdir -p $out
+    cp -r ${apple-music}/* $out
+    cp -r ${desktopItem}/* $out
+  '';
 }
