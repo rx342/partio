748c755ee4f3838b0fa80ea64af079d45c730cdf
rx342 <15484682+rx342@users.noreply.github.com>
2025-09-24 12:52:16 +0200

    fix(niri): service

--- packages/niri/default.nix
+++ packages/niri/default.nix
@@ -2,20 +2,23 @@
 
 {
   wrappers.niri = {
-    basePackage = pkgs.niri;
+    basePackage = pkgs.niri.overrideAttrs (
+      final: prev: {
+        postPatch = ''
+          ${prev.postPatch or ""}
+          substituteInPlace resources/niri.service \
+            --replace-fail '/bin/niri' "/bin/niri --config ${./config.kdl}"
+        '';
+      }
+    );
     overrideAttrs = old: {
       pname = "${old.pname}-rx342";
     };
-    programs.niri = {
-      prependFlags = [
-        "--config=${./config.kdl}"
-      ];
-      pathAdd = with pkgs; [
-        grim
-        playerctl
-        slurp
-        swaybg
-      ];
-    };
+    pathAdd = with pkgs; [
+      grim
+      playerctl
+      slurp
+      swaybg
+    ];
   };
 }
