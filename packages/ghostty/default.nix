7f7e0122c8a7ccedbc4622dbb4588ac80a842d46
rx342 <15484682+rx342@users.noreply.github.com>
2026-02-19 11:29:32 +0100

    feat(ghostty): remove small config derivation

--- packages/ghostty/default.nix
+++ packages/ghostty/default.nix
@@ -10,14 +10,4 @@
       "--config-file=${./config}"
     ];
   };
-
-  wrappers.ghostty-small = {
-    basePackage = pkgs.ghostty;
-    overrideAttrs = old: {
-      pname = "${old.pname}-rx342";
-    };
-    prependFlags = [
-      "--config-file=${./config-small}"
-    ];
-  };
 }
