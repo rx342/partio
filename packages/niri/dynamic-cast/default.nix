b96e05e106630d9dc0b96e7a2a3008f917591971
rx342 <15484682+rx342@users.noreply.github.com>
2026-05-12 20:15:58 +0200

    feat(dynamic-cast): do not need jq and use builtin json converter

--- packages/niri/dynamic-cast/default.nix
+++ packages/niri/dynamic-cast/default.nix
@@ -2,14 +2,12 @@
   writeShellApplication,
   nushell,
   niri,
-  jq,
 }:
 
 writeShellApplication {
   name = "niri-dynamic-cast";
   runtimeInputs = [
     nushell
-    jq
     niri
   ];
   text = # bash
