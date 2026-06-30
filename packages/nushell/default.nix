33a04ca23a41e2e3a206b51a6d1f7716e934a080
rx342 <15484682+rx342@users.noreply.github.com>
2026-06-30 14:23:10 +0200

    feat(nushell): add plugin support

--- packages/nushell/default.nix
+++ packages/nushell/default.nix
@@ -1,7 +1,19 @@
 { pkgs, ... }:
 
 let
+  inherit (pkgs) lib;
   nuscripts = (import ../../npins).nu_scripts.outPath;
+  plugins = with pkgs.nushellPlugins; [
+    polars
+  ];
+  msgPackz = pkgs.runCommand "nushellMsgPackz" { } ''
+    mkdir -p "$out"
+    ${pkgs.nushell}/bin/nu \
+      --plugin-config "$out/plugin.msgpackz" \
+      --commands '${
+        lib.concatStringsSep "; " (map (plugin: "plugin add ${lib.getExe plugin}") plugins)
+      }'
+  '';
   config =
     pkgs.writeText "config.nu"
       # nu
@@ -22,6 +34,8 @@ in
     prependFlags = [
       "--config"
       config
+      "--plugin-config"
+      "${msgPackz}/plugin.msgpackz"
     ];
     env.STARSHIP_CONFIG.value = ../starship/starship.toml;
     pathAdd = [
