b01b101b55785ef143b8c965b7941ee0291fd949
rx342 <15484682+rx342@users.noreply.github.com>
2026-07-16 22:26:14 +0200

    feat(mpv): modern config

--- packages/mpv/default.nix
+++ packages/mpv/default.nix
@@ -2,7 +2,13 @@
 
 {
   wrappers.mpv = {
-    basePackage = pkgs.mpv;
+    basePackage = pkgs.mpv.override {
+      scripts = with pkgs.mpvScripts; [
+        sponsorblock
+        modernz
+        thumbfast
+      ];
+    };
     overrideAttrs = old: {
       pname = "${old.pname}-rx342";
     };
